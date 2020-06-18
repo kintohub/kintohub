0bc2ba35e1b959565cdae756c6a2d1363790f053
Blake McCreary <thumpdesign@gmail.com>
2020-06-18 14:08:22 +0800

    add service types

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,10 @@
 module.exports = {
   someSidebar: {
-    Docusauruzz: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    "Getting Started": ['mdx'],
+    "Anatomy": ['anatomy/environment','anatomy/metrics','anatomy/releases','anatomy/job-history','anatomy/console-logs','anatomy/access','anatomy/domains'],
+    "Service Types": ['service-types/website','service-types/web-app','service-types/backend-api','service-types/backend-worker','service-types/job','service-types/helm'],
+    "Catalogs": ['mdx'],
+    "Examples": ['mdx'],
+    "FAQ": ['mdx'],
   },
 };
