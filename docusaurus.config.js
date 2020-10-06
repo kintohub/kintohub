a39a7db1253bf2de82ca6ed2861ac674fb550223
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-10-07 00:38:21 +0530

    :lipstick: (feat/docs landing page): Initial commit for landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,154 +1,154 @@
 module.exports = {
-  title: 'KintoHub',
-  tagline: 'Deploy Apps In Clicks, Not Weeks',
-  url: 'https://docs.kintohub.com',
-  baseUrl: '/',
-  organizationName: 'kintohub',
-  projectName: 'kintohub',
-  favicon: 'img/favicon.ico',
+  title: "KintoHub Documentation",
+  tagline:
+    "Follow out examples and guides to deploy your full stack app in no time.",
+  url: "https://docs.kintohub.com",
+  baseUrl: "/",
+  organizationName: "kintohub",
+  projectName: "kintohub",
+  favicon: "img/favicon.ico",
   themeConfig: {
     disableDarkMode: false,
-    image: 'img/kintohub-og.png',
+    image: "img/kintohub-og.png",
     prism: {
-      theme: require('./src/js/monokaiTheme.js')
+      theme: require("./src/js/monokaiTheme.js"),
     },
     navbar: {
-      title: 'KintoHub Docs',
-      image: 'img/redux-logo-landscape.png',
+      title: "KintoHub Docs",
+      image: "img/redux-logo-landscape.png",
       logo: {
-        alt: 'Kinto Logo',
-        src: 'img/kinto.svg'
+        alt: "Kinto Logo",
+        src: "img/kinto.svg",
       },
       links: [
         {
-          label: 'Getting Started',
-          to: 'getting-started/introduction',
-          position: 'right'
+          label: "Getting Started",
+          to: "getting-started/introduction",
+          position: "right",
         },
         {
-          label: 'Anatomy',
-          to: 'anatomy/environment',
-          position: 'right'
+          label: "Anatomy",
+          to: "anatomy/environment",
+          position: "right",
         },
         {
-          label: 'Examples',
-          to: 'examples/nodejs/nodejs-example',
-          position: 'right'
+          label: "Examples",
+          to: "examples/nodejs/nodejs-example",
+          position: "right",
         },
         //{ label: 'FAQ', to: 'getting-started/faq', position: 'right' },
         {
-          label: 'Need help?',
-          to: 'https://www.kintohub.com/contact-us',
-          position: 'right'
+          label: "Need help?",
+          to: "https://www.kintohub.com/contact-us",
+          position: "right",
         },
-          {
-          label: 'Login',
-          to: 'https://kintocloud.com/login',
-          position: 'right'
-        }
-      ]
+        {
+          label: "Login",
+          to: "https://kintohub.com/login",
+          position: "right",
+        },
+      ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       links: [
         {
-          title: 'KintoHub',
+          title: "KintoHub",
           items: [
             {
-              label: 'Create Account',
-              to: 'https://kintocloud.com/sign-up'
-            },
-              {
-              label: 'Pricing',
-              to: 'https://www.kintohub.com/pricing'
+              label: "Create Account",
+              to: "https://kintohub.com/sign-up",
             },
             {
-              label: 'FAQ',
-              to: 'getting-started/faq'
+              label: "Pricing",
+              to: "https://www.kintohub.com/pricing",
             },
             {
-              label: 'Contact Us',
-              to: 'https://www.kintohub.com/contact-us'
-            }
-          ]
+              label: "FAQ",
+              to: "getting-started/faq",
+            },
+            {
+              label: "Contact Us",
+              to: "https://www.kintohub.com/contact-us",
+            },
+          ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord Community',
-              href: 'https://discordapp.com/invite/E2CMjKP'
+              label: "Discord Community",
+              href: "https://discordapp.com/invite/E2CMjKP",
             },
-              {
-              label: 'Roadmap',
-              href: 'https://feedback.kintohub.com/'
+            {
+              label: "Roadmap",
+              href: "https://feedback.kintohub.com/",
             },
-              {
-              label: 'Request Features',
-              href: 'https://feedback.kintohub.com/feature-requests'
+            {
+              label: "Request Features",
+              href: "https://feedback.kintohub.com/feature-requests",
             },
-              {
-              label: 'Report a Bug',
-              href: 'https://feedback.kintohub.com/bugs'
-            }
-          ]
+            {
+              label: "Report a Bug",
+              href: "https://feedback.kintohub.com/bugs",
+            },
+          ],
         },
         {
-          title: 'Connect',
+          title: "Connect",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/kintohub/'
+              label: "GitHub",
+              href: "https://github.com/kintohub/",
             },
-              {
-              label: 'Twitter',
-              href: 'http://twitter.com/kintohub'
+            {
+              label: "Twitter",
+              href: "http://twitter.com/kintohub",
             },
-              {
-              label: 'Facebook',
-              href: 'https://www.facebook.com/KintoHub/'
+            {
+              label: "Facebook",
+              href: "https://www.facebook.com/KintoHub/",
             },
-              {
-              label: 'YouTube',
-              href: 'https://www.youtube.com/channel/UCpJJc0zcfzdHPw64BSP3GyQ'
+            {
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCpJJc0zcfzdHPw64BSP3GyQ",
             },
-              {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/kintohub/'
-            }
-          ]
-        }
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/kintohub/",
+            },
+          ],
+        },
       ],
       logo: {
-        alt: 'KintoHub Logo',
-        src: 'img/kinto.svg',
-        href: 'https://kintohub.com/'
+        alt: "KintoHub Logo",
+        src: "img/kinto.svg",
+        href: "https://kintohub.com/",
       },
-      copyright:
-        `Copyright © 2017-${new Date().getFullYear()} KintoHub Inc.`
+      copyright: `Copyright © 2017-${new Date().getFullYear()} KintoHub Inc.`,
     },
     algolia: {
       apiKey: process.env.ALGOLIA_API_KEY,
       indexName: process.env.ALGOLIA_INDEX,
-      algoliaOptions: {}
+      algoliaOptions: {},
     },
     googleAnalytics: {
-      trackingID: 'UA-112004187-1'
-    }
+      trackingID: "UA-112004187-1",
+    },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          path: 'docs',
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js')
+          path: "docs",
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
-        }
-      }
-    ]
-  ]
-}
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,92 +1,126 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
-
-const features = [
-  {
-    title: <>Our Mission</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Deploy any git repository to any cloud with ease and confidence in cost.
-      </>
-    ),
-  },
-  {
-    title: <>Next-Gen Cloud Interface</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Build, Deploy, Debug and Scale your Full Stack app on multiple clouds, in multiple regions.
-      </>
-    ),
-  },
-  {
-    title: <>We're Here For You</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        KintoHub's community is young but thriving in Discord. Join us and we'll answer any question.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       // title for OG
-      title={'Modern Fullstack App Platform'}
-      description="KintoHub is an all-in-one platform to combine and deploy your backend services, websites, cron jobs, databases and everything your app needs in one place.">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('getting-started/introduction')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
+      title={"Modern Fullstack App Platform"}
+      description="KintoHub is an all-in-one platform to combine and deploy your backend services, websites, cron jobs, databases and everything your app needs in one place."
+    >
+      <header className={styles.heroBanner}>
+        <h1 className={styles.heroBannerText}>{siteConfig.title}</h1>
+        <span className={styles.heroBannerSubText}>{siteConfig.tagline}</span>
       </header>
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
+        <section className={styles.infoCardSection}>
+          <div className={styles.infoCardsIntro}>
+            <div className={styles.infoCardSmall}>
+              <img className={styles.icon} src="/img/playlist-plus.svg"></img>
+              <h3>Introduction</h3>
+              <p>Why should you use KintoHub?</p>
             </div>
-          </section>
-        )}
+            <div className={styles.infoCardSmall}>
+              <img className={styles.icon} src="/img/code-tags.svg"></img>
+              <h3>Try a Full Stack Example</h3>
+              <p>Deploy PostgreSQL + Hasura + React.</p>
+            </div>
+            <div className={styles.infoCardSmall}>
+              <img className={styles.icon} src="/img/forum.svg"></img>
+              <h3>Join Our Discord</h3>
+              <p>Get straight answers from our developers.</p>
+            </div>
+          </div>
+          <div className={styles.infoCardsIntro}>
+            <div className={styles.infoCardLarge}>
+              <h3>Examples by Language</h3>
+              <p>Deploy code examples in your language.</p>
+              <div className={styles.pillPurple}></div>
+
+              <ul className={styles.cardContentList}>
+                <li>Node</li>
+                <li>Golang</li>
+                <li>Ruby</li>
+                <li>Python</li>
+              </ul>
+            </div>
+            <div className={styles.infoCardLarge}>
+              <h3>Explore Catalog</h3>
+              <p>Deploy instant databases, storage, etc.</p>
+              <div className={styles.pillBlue}></div>
+              <ul className={styles.cardContentList}>
+                <li>MongoDB</li>
+                <li>PostgreSQL</li>
+                <li>Minio</li>
+                <li>Redis</li>
+                <li>MySQL</li>
+                <li>MariaDB</li>
+              </ul>
+            </div>
+            <div className={styles.infoCardLarge}>
+              <h3>Learn Service Types</h3>
+              <p>How to choose a service type.</p>
+              <div className={styles.pillYellow}></div>
+              <ul className={styles.cardContentList}>
+                <li>Static Site</li>
+                <li>Web App</li>
+                <li>Backend API</li>
+                <li>Backend Worker</li>
+                <li>Job</li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.suggestion}>
+            <div className={styles.suggestionText}>
+              <h3>Deploy an Existing Project with Git</h3>
+              <p>
+                Use our Git integrations with
+                <span className={styles.link}> GitHub, </span>{" "}
+                <span className={styles.link}> Gitlab, </span>
+                or use an
+                <span className={styles.link}> import url.</span>
+              </p>
+              <img
+                className={styles.integrationIcon}
+                src="/img/github.svg"
+              ></img>
+              <img
+                className={styles.integrationIcon}
+                src="/img/gitlab.svg"
+              ></img>
+              <img className={styles.integrationIcon} src="/img/link.svg"></img>
+            </div>
+            <div className={styles.suggestionText}>
+              <h3>Connect Your Custom Domains</h3>
+              <p>
+                Once deployed, your projects can be assigned to a custom domain.
+              </p>
+
+              <Link to={useBaseUrl("anatomy/domains")}>
+                <span className={styles.link}>
+                  Learn more
+                  <img src="/img/arrow-right.svg"></img>
+                </span>
+              </Link>
+            </div>
+          </div>
+        </section>
+        <section className={styles.startDeploying}>
+          <h1>Created by fullstack developers like you.</h1>
+          <p>Play around for free. No CC required. </p>
+
+          <button className={styles.button}>
+            <img src="/img/power.svg"></img>
+            Start Deploying
+          </button>
+        </section>
       </main>
     </Layout>
   );
