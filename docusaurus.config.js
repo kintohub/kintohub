3f984c0cce957e9c68c85589dad7e5ee9a64cc5f
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-02-01 12:09:37 +0530

    :lipstick: fixed padding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ module.exports = {
             {
               html: `
                   <a href="" target="_blank" rel="noreferrer noopener" >
-                    <img style="padding-bottom:12px;" src="../img/kinto-logo.svg"  />
+                    <img style="padding-bottom:14px;" src="../img/kinto-logo.svg"  />
                   </a>
                 `,
             },
